757bee86e512e22e243300e8a7a3994974cf61a7
bullshitman <sokol.joe@gmail.com>
2019-11-06 10:35:32 +0300

    adding function formatHealthStatus

--- src/Game.kt
+++ src/Game.kt
@@ -6,19 +6,24 @@ fun main(args: Array<String>) {
     //Aura checking
     val auraVisible = isBlessed && healthPoint > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    val healthStatus = when (healthPoint){
+    val healthStatus = formatHealthStatus(healthPoint, isBlessed)
+    //Player status
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun formatHealthStatus(healthPoint: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoint) {
         100 -> "is in good condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 ->
-            if (isBlessed){
+            if (isBlessed) {
                 "has some minor wounds, but is healing quite quckly!"
-            }else{
+            } else {
                 "has some minor wounds."
             }
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    //Player status
-    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+    return healthStatus
 }
