925ec76a2e6c966920961421792f0844d7fcf3dd
bullshitman <sokol.joe@gmail.com>
2019-11-08 13:39:29 +0300

    Nullabiity chapter

--- src/SwordJuggler.kt
+++ src/SwordJuggler.kt
@@ -1,3 +1,23 @@
-fun main(args: Array<String>) {
+import java.lang.Exception
 
+fun main(args: Array<String>) {
+    var swordsJuggling: Int? = null
+    val isjugglingProficient = (1..3).shuffled().last() == 3
+    if (isjugglingProficient){
+        swordsJuggling = 2
+    }
+    try {
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+    }catch (e: Exception){
+        println(e)
+    }
+
+    println("You juggle $swordsJuggling swords!")
 }
+
+fun proficiencyCheck(swordsJuggling: Int?) {
+//    swordsJuggling ?: throw UnskilledSwordJugglerException()
+    checkNotNull(swordsJuggling, {"Player cannot juggle swords."})
+}
+class UnskilledSwordJugglerException() : IllegalStateException("Player cannot juggle swords.")
