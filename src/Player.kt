1b65610adf4b3921a0f82b04597e541efa4db674
Bullshitman <sokol.joe@gmail.com>
2019-11-21 23:10:10 +0300

    Defining Classes chapter

--- src/Player.kt
+++ src/Player.kt
@@ -1,5 +1,7 @@
 class Player {
-    val name = "madrigal"
+    var name = "madrigal"
+        get() = field.capitalize()
+    private set(value) {field = value.trim()}
     fun castFireball(numFireballs: Int = 2) =
         println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
