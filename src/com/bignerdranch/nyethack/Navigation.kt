87860609f670a83a0f12ddb46ac0fd0efff326bd
Bullshitman <sokol.joe@gmail.com>
2019-12-08 21:53:37 +0300

    Objects chapter

--- src/com/bignerdranch/nyethack/Navigation.kt
+++ src/com/bignerdranch/nyethack/Navigation.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.nyethack
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
 enum class Direction(private val coordinate: Coordinate) {
     NORTH(Coordinate(0, -1)),
@@ -9,5 +10,5 @@ enum class Direction(private val coordinate: Coordinate) {
     SOUTH(Coordinate(0, 1)),
     WEST(Coordinate(-1, 0));
     fun updateCoordinate(playerCoordinate: Coordinate) =
-        Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
+        coordinate + playerCoordinate
 }
