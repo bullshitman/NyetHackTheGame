b41c1179a1055dd57ec7c41ceb5869462381f822
Bullshitman <sokol.joe@gmail.com>
2020-04-24 13:21:12 +0300

    Interfaces and Abstract classes chapter done

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -1,104 +1,24 @@
-package com.bignerdranch.nyethack
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    val isBlessed = true
+    var healthPoint = 100
+    val isImmortal = false
+    val auraVisible = isBlessed && healthPoint > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
 
-import com.sun.javafx.scene.traversal.TopMostTraversalEngine
-import java.lang.Exception
-import java.lang.IllegalStateException
-import kotlin.system.exitProcess
 
-fun main() {
-
-    Game.play()
+    val healthStatus = when (healthPoint) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt"
+        else -> "is in awful condition!"
+    }
+    println("(Aura: $auraColor) " +
+    "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
 }
-object Game {
-    private val player = Player("Madrigal")
-    private var currentRoom: Room = TownSquare()
-    private val worldMap = listOf(
-        listOf(currentRoom, Room("Tavern"), Room("Back Room")),
-        listOf(Room("Long Corridor"), Room("Generic Room"))
-    )
-    init {
-        println("Welcome, adventurer.")
-        player.castFireball()
-    }
-    fun play() {
-        while (true) {
-            println(currentRoom.description())
-            println(currentRoom.load())
-
-            printPlayerStatus(player)
-            print("> Enter your command: ")
-            println(GameInput(readLine()).processCommand())
-        }
-    }
-    private fun printPlayerStatus(player: Player) {
-        println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-        println("${player.name} ${player.formatHealthStatus()}")
-    }
-    private class GameInput(arg: String?) {
-        private val input = arg ?: ""
-        val command = input.split(" ")[0]
-        val argument = input.split(" ").getOrElse(1, { "" })
-
-        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
-
-        fun processCommand() = when(command.toLowerCase()) {
-            "move" -> move(argument)
-            "fight" -> fight()
-            "quit" -> quit()
-            "ring" -> currentRoom.ringBell()
-            else -> commandNotFound()
-        }
-    }
-    private fun move(directionInput: String) =
-        try {
-            val direction = Direction.valueOf(directionInput.toUpperCase())
-            val newPosition = direction.updateCoordinate(player.currentPosition)
-            if (!newPosition.isInBounds) {
-                throw IllegalStateException("$direction is out of bounds.")
-            }
-            val newRoom = worldMap[newPosition.y][newPosition.x]
-            player.currentPosition = newPosition
-            currentRoom = newRoom
-            drawMap(currentRoom)
-
-            "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
-        } catch (e: Exception) {
-            "Invalid direction: $directionInput."
-        }
-    private fun fight() = currentRoom.monster?.let {
-        while (player.healthPoints > 0 && it.healthPoints > 0){
-            slay(it)
-            Thread.sleep(1000)
-        }
-        "Combat complete."
-    } ?: "There is nothing here to fight."
-    private fun slay(monster: Monster) {
-        println("${monster.name} did ${monster.attack(player)} damage!")
-        println("${player.name} did ${player.attack(monster)} damage!")
-        if (player.healthPoints <= 0) {
-            println(">>>> You have been defeated! Thanks for playing. <<<<")
-            quit()
-        }
-        if (monster.healthPoints <= 0) {
-            println(">>>> ${monster.name} has beed defeated!<<<<")
-            currentRoom.monster = null
-        }
-    }
-    private fun drawMap(currentRoom: Room) {
-        for (rooms in worldMap) {
-            for (room in rooms) {
-                if (room == currentRoom) {
-                    print("X")
-                } else {
-                    print("O")
-                }
-            }
-            print("\n")
-        }
-    }
-    private fun quit() {
-        println("Game over, see you next time")
-        exitProcess(1)
-    }
-}
-
