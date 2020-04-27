0cde82176b87313ef653a3cc4bb582c0d30c0b5f
Bullshitman <sokol.joe@gmail.com>
2020-04-27 15:33:44 +0300

    Interfaces and Abstract classes chapter done

--- src/com/bignerdranch/nyethack/SwordJuggler.kt
+++ src/com/bignerdranch/nyethack/SwordJuggler.kt
@@ -1,24 +1,24 @@
-package com.bignerdranch.nyethack
-
 import java.lang.Exception
+import java.lang.IllegalStateException
 
 fun main(args: Array<String>) {
-    var swordsJuggling: Int? = null
-    val isjugglingProficient = (1..3).shuffled().last() == 3
-    if (isjugglingProficient){
-        swordsJuggling = 2
+    var swordJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient){
+        swordJuggling = 2
     }
     try {
-        proficiencyCheck(swordsJuggling)
-        swordsJuggling = swordsJuggling!!.plus(1)
-    }catch (e: Exception){
+        proficientCheck(swordJuggling)
+        swordJuggling = swordJuggling!!.plus(1)
+    } catch (e: Exception) {
         println(e)
     }
 
-    println("You juggle $swordsJuggling swords!")
+    println("You juggle $swordJuggling swords!")
 }
 
-fun proficiencyCheck(swordsJuggling: Int?) {
-    checkNotNull(swordsJuggling, {"com.bignerdranch.nyethack.Player cannot juggle swords."})
+fun proficientCheck(swordJuggling: Int?) {
+    checkNotNull(swordJuggling, {"Player cannot juggle swords."})
 }
-class UnskilledSwordJugglerException() : IllegalStateException("com.bignerdranch.nyethack.Player cannot juggle swords.")
+class UnskilledSwordJugglerException():
+        IllegalStateException("Player cannot juggle swords.")
