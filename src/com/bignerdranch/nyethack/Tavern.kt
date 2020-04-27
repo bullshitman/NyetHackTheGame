538be179f7d88e93e2151b42c07dd26d5361f56d
Bullshitman <sokol.joe@gmail.com>
2020-04-27 19:46:11 +0300

    Interfaces and Abstract classes chapter done

--- src/com/bignerdranch/nyethack/Tavern.kt
+++ src/com/bignerdranch/nyethack/Tavern.kt
@@ -1,7 +1,15 @@
+import java.math.RoundingMode
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Taernyl's Folly"
+var playerGold = 10
+var playerSilver = 10
+var playerDracCoins = 5.0
+var gallons = 5.0
+var pintsCount = gallons / 0.125
 fun main(args: Array<String>) {
     placeOrder("Shandy,Dragon's Breath,5.91")
-//    placeOrder("elixir,Shirley's Temple,4.12")
+
 }
 
 private fun placeOrder(menuData: String) {
@@ -11,6 +19,7 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
+    preformPurchase(price.toDouble())
 //    val phrase = "Ah, delicious $name"
 //    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
     val phrase = if (name == "Dragon's Breath") {
@@ -19,6 +28,49 @@ private fun placeOrder(menuData: String) {
         "Madrigal says: Thanks for the $name"
     }
     println(phrase)
+}
+
+private fun preformPurchase(price: Double) {
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver/100.0)
+    val totalPurseCoins = playerDracCoins * 1.43
+    println("Total purse: $totalPurse")
+    println("Total purse: ${"%.4f".format(totalPurseCoins)}")
+    println("Purchasing item for $price")
+    if (checkPlayerBalance(price, totalPurse)) {
+        val remainingBalance = totalPurse - price
+        pintsCount -= 1
+        println("remaining balance: ${"%.2f".format(remainingBalance)}")
+        val remainingGold = remainingBalance.toInt()
+        val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+        playerGold = remainingGold
+        playerSilver = remainingSilver
+        displayBalance()
+    } else {
+        println("Not enough money.")
+    }
+
+    if (checkPlayerBalance(price, totalPurseCoins)) {
+        val remainingBalance = totalPurseCoins - price
+        pintsCount -= 1
+        println("remaining balance: ${"%.2f".format(remainingBalance)}")
+        val remainingCoins = remainingBalance / 1.43
+
+        playerDracCoins = remainingCoins
+        displayBalance()
+    } else {
+        println("Not enough money.")
+    }
+}
+
+private fun checkPlayerBalance(price: Double, totalPurse: Double): Boolean {
+    return (totalPurse - price) > 0
+}
+
+private fun displayBalance() {
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
+    println("Still have $pintsCount of Dragon's Breath.")
+    println("Player's purse balance: coins: ${"%.4f".format(playerDracCoins)}\"")
 }
 
 private fun toDragonSpeak(phrase: String) =
