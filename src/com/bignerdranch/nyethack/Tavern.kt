5a1a5be5c8821d10bd4ca4bb8b0fab24e4d2bd22
Bullshitman <sokol.joe@gmail.com>
2020-04-28 22:03:30 +0300

    Interfaces and Abstract classes chapter done

--- src/com/bignerdranch/nyethack/Tavern.kt
+++ src/com/bignerdranch/nyethack/Tavern.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.lang.StringBuilder
 import kotlin.math.roundToInt
 
 const val TAVERN_NAME = "Taernyl's Folly"
@@ -8,6 +9,8 @@ val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
 val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
 val uniqPatrons = mutableSetOf<String>()
 val menuList = File("data/tavern_menu_data.txt").readText().split("\n")
+val setsOfDrinks = mutableSetOf<String>()
+val listWidth = 40
 fun main(args: Array<String>) {
     (0..9).forEach {
         val first = patronList.shuffled().first()
@@ -16,11 +19,54 @@ fun main(args: Array<String>) {
         uniqPatrons += name
     }
     println(uniqPatrons)
-    var orderCount = 0
-    while (orderCount <= 9) {
-        placeOrder(uniqPatrons.shuffled().first(), menuList.shuffled().first())
-        orderCount++
+    val welcomeString = "Welcome to $TAVERN_NAME"
+    val stars = prepareOutString("*", (listWidth - welcomeString.length - 2) / 2)
+    println("$stars $welcomeString $stars")
+//    menuList.forEach { data ->
+//        val (_, name, price) = data.split(',')
+//        val words = name.split(" ").joinToString { it.capitalize()}.trimEnd()
+//        val menuLine = "$words${prepareOutString(".", listWidth - words.length - price.trimMargin().length)}${price.trimMargin()}"
+//        println(menuLine)
+//    }
+    menuList.forEach { data ->
+        val (type, _, _) = data.split(',')
+        setsOfDrinks += type
     }
+    setsOfDrinks.forEach { cathegory ->
+        val cathegotyString = prepareOutString(" ", (listWidth - cathegory.length - 4)/2)
+        println("$cathegotyString~[$cathegory]~$cathegotyString")
+        menuList.forEach { data ->
+            val (type, name, price) = data.split(',')
+            if (cathegory == type) {
+                val words = name.split(" ").joinToString { it.capitalize()}.trimEnd()
+                val menuString = "$words${prepareOutString(".", listWidth - words.length - price.trimMargin().length)}${price.trimMargin()}"
+                println(menuString)
+            }
+        }
+    }
+    println("$stars $welcomeString $stars")
+    menuList.forEach { data ->
+        val (_, name, price) = data.split(',')
+
+        val words = name.split(" ").joinToString { it.capitalize()}.trimEnd()
+
+        val menuString = "$words${prepareOutString(".", listWidth - words.length - price.trimMargin().length)}${price.trimMargin()}"
+        println(menuString)
+    }
+
+    var orderCount = 0
+//    while (orderCount <= 9) {
+//        placeOrder(uniqPatrons.shuffled().first(), menuList.shuffled().first())
+//        orderCount++
+//    }
+}
+
+private fun prepareOutString(symbol: String, count: Int): String {
+    var newLine = StringBuilder()
+    repeat(count) {
+        newLine.append(symbol)
+    }
+    return newLine.toString()
 }
 
 private fun placeOrder(patronName: String, menuData: String) {
