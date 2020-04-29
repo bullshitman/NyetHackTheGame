28d713fea75a22ced1f7e194ebd95b67067cbb6c
Bullshitman <sokol.joe@gmail.com>
2020-04-29 19:52:25 +0300

    Interfaces and Abstract classes chapter done

--- src/com/bignerdranch/nyethack/Game.kt
+++ src/com/bignerdranch/nyethack/Game.kt
@@ -1,41 +1,16 @@
 fun main(args: Array<String>) {
-    val name = "Madrigal"
-    val isBlessed = true
-    var healthPoint = 100
-    val isImmortal = false
-    val auraColor = auraColor(isBlessed, healthPoint, isImmortal)
-    val healthStatus = formatHealthStatus(healthPoint, isBlessed)
-    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-    castFireBall()
+    val player =  Player()
+    printPlayerStatus(player)
+    player.castFireball()
+
 }
 
 private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
+    player: Player
 ) {
     println(
-        "(Aura: $auraColor) " +
-                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+        "(Aura: ${player.auraColor()} " +
+                "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
     )
-    println("$name $healthStatus")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
-
-private fun auraColor(isBlessed: Boolean, healthPoint: Int, isImmortal: Boolean) =
-    if (isBlessed && healthPoint > 50 || isImmortal) "GREEN" else "NONE"
-
-private fun formatHealthStatus(healthPoint: Int, isBlessed: Boolean) =
-     when (healthPoint) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds, but is healing quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt"
-        else -> "is in awful condition!"
-    }
-private fun castFireBall(numFireBalls: Int = 2) =
-    println("A glass of Fireball springs into existence. (x$numFireBalls)")
--- src/com/bignerdranch/nyethack/Player.kt
+++ src/com/bignerdranch/nyethack/Player.kt
@@ -1,61 +1,29 @@
-package com.bignerdranch.nyethack
-
-import java.io.File
-
-class Player (_name: String, override var healthPoints: Int = 100, val isBlessed: Boolean, private val isImmortal: Boolean):Fightable {
-    override val direCount: Int = 3
-    override val diceSides: Int = 6
-    override fun attack(opponent: Fightable): Int {
-        val damageDealt = if (isBlessed) {
-            damageRoll * 2
-        }else{
-            damageRoll
+class Player {
+    var name = "madrigal"
+        get() = field.capitalize()
+        private set (value) {
+            field = value.trim()
         }
-        opponent.healthPoints -=damageDealt
-        return damageDealt
-    }
+    val isBlessed = true
+    var healthPoint = 89
+    private val isImmortal = false
 
-    var name = _name
-        get() = "${field.capitalize()} of $hometown"
-    private set(value) {field = value.trim()}
-    private val hometown by lazy {selectHomeTown()}
-    var currentPosition = Coordinate(0 , 0)
+    fun auraColor() =
+        if (isBlessed && healthPoint > 50 || isImmortal) "GREEN" else "NONE"
 
-    init {
-        require(healthPoints > 0, {"healthPoints must be greater than zero."})
-        require(name.isNotBlank(), {"Player must have a name"})
-    }
-    constructor(name: String): this(name,
-        isBlessed = true,
-        isImmortal = false){
-        if (name.toLowerCase() == "kar") healthPoints = 40
-    }
-
-    fun auraColor(): String {
-        val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-        val auraColor = if (auraVisible) "GREEN" else "NONE"
-        return auraColor
-    }
-
-    private fun selectHomeTown() = File("data/towns.txt")
-        .readText()
-        .split("\r\n")
-        .shuffled()
-        .first()
-
-    fun formatHealthStatus() = when (healthPoints) {
-        100 -> "is in good condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 ->
-            if (isBlessed) {
-                "has some minor wounds, but is healing quite quckly!"
+    fun formatHealthStatus() =
+        when (healthPoint) {
+            100 -> "is in excellent condition!"
+            in 90..99 -> "has a few scratches."
+            in 75..89 -> if (isBlessed) {
+                "has some minor wounds, but is healing quickly!"
             } else {
                 "has some minor wounds."
             }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
+            in 15..74 -> "looks pretty hurt"
+            else -> "is in awful condition!"
+        }
 
-    fun castFireball(numFireballs: Int = 2) =
-        println("A glass of Fireball springs into existence. (x$numFireballs)")
+    fun castFireball(numFireBalls: Int = 2) =
+        println("A glass of Fireball springs into existence. (x$numFireBalls)")
 }
